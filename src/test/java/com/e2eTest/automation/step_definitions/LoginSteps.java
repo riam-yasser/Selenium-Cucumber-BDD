764870aa20e14115eedcf11bfd43696c2122b3f9
unknown <riamyasser1@gmail.com>
2025-12-22 22:51:14 +0100

    Update Project

---
+++ src/test/java/com/e2eTest/automation/step_definitions/LoginSteps.java
@@ -0,0 +1,33 @@
+package com.e2eTest.automation.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+
+	@Given("Je me connecte sur le site Web Demo Shop")
+	public void jeMeConnecteSurLeSiteWebDemoShop() {
+	}
+	
+	@When("Je clique sur le bouton Log in")
+	public void jeCliqueSurLeBoutonLogIn() {
+	}
+	
+	@When("Je saisis une adresse mail")
+	public void jeSaisisUneAdresseMail() {
+	}
+	
+	@When("Je saisis un mot de passe")
+	public void jeSaisisUnMotDePasse() {
+	}
+	
+	@When("Je clique sur le bouton Login")
+	public void jeCliqueSurLeBoutonLogin() {
+	}
+	
+	@Then("Je me redirige vers la page home {string}")
+	public void jeMeRedirigeVersLaPageHome(String string) {
+	}
+
+}
